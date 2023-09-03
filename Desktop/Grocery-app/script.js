09b0a0fcea82be9b271bc89f7172eb84139dd583
Drin Bratotini <drin.bratotini@gmail.com>
2023-09-03 11:24:07 -0400

    fixed button bug

--- Desktop/Grocery-app/script.js
+++ Desktop/Grocery-app/script.js
@@ -38,17 +38,26 @@ function addItem(e) {
 
 allButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
+    // console.log(button);
     // put message and data type into variables.
     const message = e.target.getAttribute("data-message");
     const alertType = e.target.getAttribute("data-alert-type");
+
+    console.log(button, message, alertType);
     // get inputValue to check submit button(2 outcomes)
     const inputValue = grocery.value;
-    // if value is empty, display failure function
-    if (inputValue.trim() == "") {
+    // if value is empty, display failure function && target is our submit button
+    if (
+      inputValue.trim() == "" &&
+      e.target.getAttribute("class") === "submit-btn my-button"
+    ) {
       displayAlert("Empty Value!", "alert-danger");
     }
-    // if value is not empty, submit item accordingly
-    else if (inputValue.trim()) {
+    // if value is not empty, submit item accordingly && target is our submit button
+    else if (
+      inputValue.trim() &&
+      e.target.getAttribute("class") === "submit-btn my-button"
+    ) {
       displayAlert("Item submitted Successfully!", "alert-success");
     }
     // else statement for the other 3 buttons that only have 1 outcome
@@ -76,9 +85,16 @@ Optional: start off with the item holders being #hidden
  If User clicks edit button, edit <p> tag
  If theres a lot of items, increase container size as needed
  If Clear-BTN is clicked, remove all lists
- 
+ Clicking the edit button turn the item text into an input field for editing, once user clicks edit, it changes back to a <p> tag
+ Optional: add confirmation button when user clicks Clear Button(In case they click accidentally/ or add Undo button)
+
 
 */
+//Function to create the container with the item that is added by User/ alongside Edit and Delete Buttons
+
+// document.createElement("");
+// let createItemContainer = () => {};
+
 // LOCAL STORAGE
 
 //SETUP ITEMS
