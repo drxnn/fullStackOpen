352cb00df0b84aab29e002eeb05ac4c2730915e2
Drin Bratotini <drin.bratotini@gmail.com>
2024-04-12 19:28:40 -0400

    tests finished

--- Desktop/fullStackOpen/part3/notes-backend/models/users.js
+++ Desktop/fullStackOpen/part3/notes-backend/models/users.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true, // this ensures the uniqueness of username
+  },
   name: String,
   passwordHash: String,
   notes: [
@@ -17,10 +21,11 @@ userSchema.set("toJSON", {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
-    // the password hash should not be revealed
+    // the passwordHash should not be revealed
     delete returnedObject.passwordHash;
   },
 });
 
 const User = mongoose.model("User", userSchema);
+
 module.exports = User;
