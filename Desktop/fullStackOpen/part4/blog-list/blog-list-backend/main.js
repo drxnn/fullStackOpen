40a2603a1d563155de490a8b4402d42868164659
Drin Bratotini <drin.bratotini@gmail.com>
2024-06-21 15:21:28 -0400

    blog list users added

--- Desktop/fullStackOpen/part4/blog-list/blog-list-backend/main.js
+++ Desktop/fullStackOpen/part4/blog-list/blog-list-backend/main.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 const middleware = require("./utils/middleware");
 const Blog = require("./models/blogs");
 // const colors = require("colors");
@@ -37,6 +38,7 @@ app.get("/info", async (req, res, next) => {
 });
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
