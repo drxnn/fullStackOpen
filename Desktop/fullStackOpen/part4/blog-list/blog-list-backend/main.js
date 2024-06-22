c527c41bf1dd010f7ac46820fd6b5b97f25024b1
Drin Bratotini <drin.bratotini@gmail.com>
2024-06-22 15:48:24 -0400

    added token authentication

--- Desktop/fullStackOpen/part4/blog-list/blog-list-backend/main.js
+++ Desktop/fullStackOpen/part4/blog-list/blog-list-backend/main.js
@@ -7,6 +7,7 @@ const logger = require("./utils/logger");
 const config = require("./utils/config");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const middleware = require("./utils/middleware");
 const Blog = require("./models/blogs");
 // const colors = require("colors");
@@ -39,6 +40,7 @@ app.get("/info", async (req, res, next) => {
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
