65e76682004ae350860c38e0c841ccead3a7c79b
Drin Bratotini <drin.bratotini@gmail.com>
2024-06-23 14:41:23 -0400

    exercise 4.19

--- Desktop/fullStackOpen/part4/blog-list/blog-list-backend/controllers/blogs.js
+++ Desktop/fullStackOpen/part4/blog-list/blog-list-backend/controllers/blogs.js
@@ -1,6 +1,7 @@
 const Blog = require("../models/blogs");
 const User = require("../models/users");
 const blogsRouter = require("express").Router();
+const jwt = require("jsonwebtoken");
 
 blogsRouter.get("/", async (request, response, next) => {
   try {
@@ -37,12 +38,28 @@ blogsRouter.delete("/:id", async (request, response, next) => {
   response.status(204).end();
 });
 
+//Modify adding new blogs so that it is only possible if a valid token is sent with the HTTP POST request. The user identified by the token is designated as the creator of the blog.//
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
+
 blogsRouter.post("/", async (request, response, next) => {
   if (!request.body.likes) {
     request.body.likes = 0;
   }
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+
   const { title, author, url, likes } = request.body;
-  const user = await User.findById(request.body.userId);
+  const user = await User.findById(decodedToken.id);
 
   if (!user) {
     return res.status(400).json({ error: "No users found in the database" });
