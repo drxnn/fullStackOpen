df1e0c27282d0012ddcfbe5952776985188bf6d8
Drin Bratotini <drin.bratotini@gmail.com>
2024-07-06 13:35:10 -0400

    starting part 5

---
+++ Desktop/fullStackOpen/part5/notes-front-part5/Components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Notes Application</em>
+    </div>
+  );
+};
+
+export default Footer;
