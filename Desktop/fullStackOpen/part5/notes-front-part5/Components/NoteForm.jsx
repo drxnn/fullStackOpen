f3203dbfa822cf9599b11539139c97c2fdec1401
Drin Bratotini <drin.bratotini@gmail.com>
2024-07-29 12:22:09 -0400

    fixed a test

--- Desktop/fullStackOpen/part5/notes-front-part5/Components/NoteForm.jsx
+++ Desktop/fullStackOpen/part5/notes-front-part5/Components/NoteForm.jsx
@@ -13,14 +13,17 @@ const NoteForm = ({ createNote }) => {
     setNewNote("");
   };
   return (
-    <div>
+    <div className="formDiv">
       <h2>Create a new note</h2>
 
       <form onSubmit={addNote}>
         <input
           value={newNote}
           onChange={(event) => setNewNote(event.target.value)}
+          placeholder="write note content here"
         />
+        <input value="something" onChange={() => console.log("l")} />
+
         <button type="submit">save</button>
       </form>
     </div>
