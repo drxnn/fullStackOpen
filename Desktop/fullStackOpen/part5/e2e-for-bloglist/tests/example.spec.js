9e81169a4e1613094142d6d50c4be7861ef2a80c
Drin Bratotini <drin.bratotini@gmail.com>
2024-08-15 20:13:00 -0400

    tests

--- Desktop/fullStackOpen/part5/e2e-for-bloglist/tests/example.spec.js
+++ Desktop/fullStackOpen/part5/e2e-for-bloglist/tests/example.spec.js
@@ -3,12 +3,15 @@ const { test, expect, describe, beforeEach } = require("@playwright/test");
 
 describe("blog app", () => {
   beforeEach(async ({ page, request }) => {
-    // await request.post("http://localhost:3003/api/reset");
-    // await request.post("http://localhost:3003/api/users", {
-    //   username: "Enzo",
-    //   password: "enzoenzo",
-    // });
-    await page.goto("http://localhost:5174");
+    await request.post("http://localhost:3003/api/testing/reset");
+    await request.post("http://localhost:3003/api/users", {
+      data: {
+        username: "testingUser",
+        name: "testname",
+        password: "testtest",
+      },
+    });
+    await page.goto("http://localhost:5173");
   });
 
   test("Login form is shown", async ({ page }) => {
@@ -17,17 +20,27 @@ describe("blog app", () => {
 
   describe("Login tests ", () => {
     test("Fails with incorrect credentials", async ({ page }) => {
-      await page.getByTestId("username-input").fill("Enzo");
-      await page.getByTestId("password-input").fill("wrongpassword");
+      await page.getByTestId("username-input").fill("testingUser");
+      await page.getByTestId("password-input").fill("wrongPassword");
       await page.getByRole("button", { name: "login" }).click();
       await expect(page.getByText(/Wrong credentials/i)).toBeVisible();
     });
 
     test("succeeds with correct credentials", async ({ page }) => {
-      await page.getByTestId("username-input").fill("Enzo");
-      await page.getByTestId("password-input").fill("enzoenzo");
+      await page.getByTestId("username-input").fill("testingUser");
+      await page.getByTestId("password-input").fill("testtest");
       await page.getByRole("button", { name: "login" }).click();
       await expect(page.getByText(/Successful/i)).toBeVisible();
+    });
+  });
+  describe("When logged in", () => {
+    beforeEach(async ({ page }) => {
+      // ...
+    });
+
+    test("a new blog can be created", async ({ page }) => {
+      await page.getByTestId("username-input").fill("testingUser");
+      await page.getByTestId("password-input").fill("testtest");
     });
   });
 });
