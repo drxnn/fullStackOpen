bd0670fa270f6af4a85a32fe1aa084aa4530f476
Drin Bratotini <drin.bratotini@gmail.com>
2024-08-28 10:36:56 -0400

    part 6 done

--- Desktop/fullStackOpen/part6/part6-app/src/main.jsx
+++ Desktop/fullStackOpen/part6/part6-app/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import { noteReducer } from "./reducers/noteReducer";
+
+const store = createStore(noteReducer);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
