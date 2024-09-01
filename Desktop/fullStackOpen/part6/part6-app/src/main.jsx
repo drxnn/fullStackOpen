9cf40996782217e00f00cccff0e4608c9e776db3
Drin Bratotini <drin.bratotini@gmail.com>
2024-09-01 11:49:59 -0400

    part 6.b done

--- Desktop/fullStackOpen/part6/part6-app/src/main.jsx
+++ Desktop/fullStackOpen/part6/part6-app/src/main.jsx
@@ -2,19 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import "./index.css";
 
-import { noteReducer } from "./reducers/noteReducer";
+import noteReducer from "./reducers/noteReducer";
 
 import filterReducer from "./reducers/filterReducer";
-
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer,
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer,
+  },
 });
 
-const store = createStore(reducer);
 console.log(store.getState());
 
 createRoot(document.getElementById("root")).render(
