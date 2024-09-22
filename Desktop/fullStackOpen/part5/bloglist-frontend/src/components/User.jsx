056ea390498b038e6ef491ac003eff9d65ff4fb4
Drin Bratotini <drin.bratotini@gmail.com>
2024-09-22 18:42:13 -0400

    .

--- Desktop/fullStackOpen/part5/bloglist-frontend/src/components/User.jsx
+++ Desktop/fullStackOpen/part5/bloglist-frontend/src/components/User.jsx
@@ -9,6 +9,7 @@ const User = ({ user }) => {
   return (
     <div>
       <h2>{user.name}</h2>
+      <h4>Blogs added from user:</h4>
       <ul>
         {user.blogs.map((blog) => (
           <li key={blog.id}>{blog.title}</li>
