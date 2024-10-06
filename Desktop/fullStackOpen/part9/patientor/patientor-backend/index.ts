6ccec1a62bc1de125bb2b38cf5b082758c90a605
Drin Bratotini <drin.bratotini@gmail.com>
2024-10-06 18:31:03 -0400

    9.11

--- Desktop/fullStackOpen/part9/patientor/patientor-backend/index.ts
+++ Desktop/fullStackOpen/part9/patientor/patientor-backend/index.ts
@@ -1,12 +1,33 @@
 import express from "express";
 const app = express();
 import cors from "cors";
+import diagnosesData from "./data/diagnoses";
+import { NonSensitivePatientData } from "./data/patients";
+// console.log(diagnosesData);
+import patientData from "./data/patients";
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/api/ping", (_request, response) => {
   response.send("HELLO THERE SIR");
+});
+
+app.get("/api/diagnoses", (_request, response) => {
+  response.send(diagnosesData);
+});
+
+app.get("/api/patients", (_request, response) => {
+  const dataToReturn: NonSensitivePatientData[] = patientData.map((el) => {
+    return {
+      id: el.id,
+      name: el.name,
+      dateOfBirth: el.dateOfBirth,
+      gender: el.gender,
+      occupation: el.occupation,
+    };
+  });
+  response.send(dataToReturn);
 });
 
 const PORT = 3000;
