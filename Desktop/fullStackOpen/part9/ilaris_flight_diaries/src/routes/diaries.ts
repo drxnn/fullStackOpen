d7bc2b2b4ed2ff704719d69022a7849287e169ab
Drin Bratotini <drin.bratotini@gmail.com>
2024-10-07 17:42:50 -0400

    9.c

--- Desktop/fullStackOpen/part9/ilaris_flight_diaries/src/routes/diaries.ts
+++ Desktop/fullStackOpen/part9/ilaris_flight_diaries/src/routes/diaries.ts
@@ -3,13 +3,34 @@ import diaryService from "../services/diaryService";
 import { Response } from "express";
 import { NonSensitiveDiaryEntry } from "../types";
 const router = express.Router();
+import toNewDiaryEntry from "../utils";
 
 router.get("/", (_req, res: Response<NonSensitiveDiaryEntry[]>) => {
   res.send(diaryService.getNonSensitiveEntries());
 });
 
-router.post("/", (_req, res) => {
-  res.send("Saving a diary!");
+router.get("/:id", (req, res) => {
+  const diary = diaryService.findById(Number(req.params.id));
+  if (diary) {
+    res.send(diary);
+  } else {
+    res.status(404).send({ error: "Nothing found" });
+  }
+});
+
+router.post("/", (req, res) => {
+  try {
+    const newDiaryEntry = toNewDiaryEntry(req.body);
+
+    const addedEntry = diaryService.addDiary(newDiaryEntry);
+    res.json(addedEntry);
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong.";
+    if (error instanceof Error) {
+      errorMessage += " Error: " + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default router;
